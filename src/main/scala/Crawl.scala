739cf7c44a8227b3046b49cd3326e84943889c7f
Zulek <zulek24@gmail.com>
2017-08-27 11:03:24 +0800

    Didn't connect with master spark on vagrant

--- src/main/scala/Crawl.scala
+++ src/main/scala/Crawl.scala
@@ -5,7 +5,6 @@ import com.github.scribejava.core.model.OAuthRequest
 import com.github.scribejava.core.model.Verb
 import com.github.scribejava.apis.VkontakteApi
 import com.github.scribejava.core.oauth.OAuth20Service
-import com.redis.RedisClient
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.json4s._
 import org.json4s.native.JsonMethods._
@@ -17,42 +16,45 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Crawl {
-
-  val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("KafkaCrawler")
-  val ssc = new StreamingContext(sparkConf, Seconds(2))
-
-  val kafkaStream: DStream[String] = {
-    val sparkStreamingConsumerGroup = "test"
-    val kafkaParams = Map(
-      "zookeeper.connect" -> "localhost:2181",
-      "group.id" -> "test",
-      "zookeeper.connection.timeout.ms" -> "1000")
-    val inputTopic = "test"
-    val numPartitionsOfInputTopic = 5
-    val streams = (1 to numPartitionsOfInputTopic) map { _ =>
-      KafkaUtils.createStream(ssc, kafkaParams("zookeeper.connect"), kafkaParams("group.id"), Map(inputTopic -> 1), StorageLevel.MEMORY_ONLY_SER).map(_._2)
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      System.err.println("Usage: Crawl <apiKey / ID ВК приложения> <apiSecret / Защищённый ключ>")
+      System.exit(1)
     }
-    val unifiedStream = ssc.union(streams)
-    val sparkProcessingParallelism = 1 // You'd probably pick a higher value than 1 in production.
-    unifiedStream.repartition(sparkProcessingParallelism)
-  }
+    val sparkConf: SparkConf = new SparkConf().setAppName("KafkaCrawler")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
 
-  val brokerList = "localhost:9092"
-  val outputTopic = "test"
-  val props = new util.HashMap[String, Object]()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
-  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-    "org.apache.kafka.common.serialization.StringSerializer")
-  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-    "org.apache.kafka.common.serialization.StringSerializer")
-  val kafkaSink: Broadcast[KafkaSink] = ssc.sparkContext.broadcast(KafkaSink(props))
+    val kafkaStream: DStream[String] = {
+      val sparkStreamingConsumerGroup = "test"
+      val kafkaParams = Map(
+        "zookeeper.connect" -> "localhost:2181",
+        "group.id" -> "test",
+        "zookeeper.connection.timeout.ms" -> "1000")
+      val inputTopic = "test"
+      val numPartitionsOfInputTopic = 5
+      val streams = (1 to numPartitionsOfInputTopic) map { _ =>
+        KafkaUtils.createStream(ssc, kafkaParams("zookeeper.connect"), kafkaParams("group.id"), Map(inputTopic -> 1), StorageLevel.MEMORY_ONLY_SER).map(_._2)
+      }
+      val unifiedStream = ssc.union(streams)
+      val sparkProcessingParallelism = 1 // You'd probably pick a higher value than 1 in production.
+      unifiedStream.repartition(sparkProcessingParallelism)
+    }
 
-  def start(key:String, secret: String): Unit = {
+    val brokerList = "localhost:9092"
+    val outputTopic = "test"
+    val props = new util.HashMap[String, Object]()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringSerializer")
+    val kafkaSink: Broadcast[KafkaSink] = ssc.sparkContext.broadcast(KafkaSink(props))
+
 
     // Define the actual data flow of the streaming job
     kafkaStream
       .flatMap(id => {
-        val service: OAuth20Service = (new ServiceBuilder).apiKey(key).apiSecret(secret).build(VkontakteApi.instance)
+        val service: OAuth20Service = (new ServiceBuilder).apiKey(args(0)).apiSecret(args(1)).build(VkontakteApi.instance)
         val friendsLists = {
           val request = new OAuthRequest(Verb.GET, s"https://api.vk.com/method/friends.get?user_id=$id&v=5.68")
           val response = service.execute(request)
@@ -79,7 +81,3 @@ object Crawl {
     ssc.awaitTermination()
   }
 }
-
-//object Crawl {
-//  def apply(apiKey: String, apiSecret: String): Crawl = new Crawl(apiKey, apiSecret)
-//}
